cf42e5da69deff191b682f1c595c75aad00dae78
Raj Shekar Patha <rajashekarpatha07@gmail.com>
2025-03-12 19:48:54 +0530

    Learned Professional way to connect to DB

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import  dotenv  from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+/*const app = express();
+
+( async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("Error",(error)=>{
+            console.log("Error",error)
+            throw error
+        });
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`app is listing on port ${process.env.PORT}`);
+            
+        })
+    } catch (error) {
+        console.error("There is a error", error)
+    }   
+
+})()
+    */
