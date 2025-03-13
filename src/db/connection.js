7f9d0672c0a1b363db981bc975f9127e37aec902
Raj Shekar Patha <rajashekarpatha07@gmail.com>
2025-03-13 16:36:49 +0530

    Made Custom Api Responses and Error Handling

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,18 +1,20 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-const connectDB = async()=>{
+const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n MongoDB connected !! DB HOST ${connectionInstance.connection.host}`);
-        
-        
+        if (!process.env.MONGODB_URI) {
+            throw new Error("MONGODB_URI is not defined in environment variables.");
+        }
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`\nMongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("Error Connecting in Db: ",error);
-        process.exit(1)
-        
+        console.error("Error Connecting to DB:", error);
+        process.exit(1);
     }
-}
+};
 
-
-export default connectDB
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import  dotenv  from "dotenv";
+import express from "express";
 import connectDB from "./db/connection.js";
+
+const app = express()
 
 dotenv.config({
     path: './env'
@@ -7,6 +10,16 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running on port ${process.env.PORT||8000}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("Err in connecting to db", err);
+    
+})
 
 
 
