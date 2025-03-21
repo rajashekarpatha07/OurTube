3c2a9815c8cd242e8845162e140a18ef20cb09d1
Raj Shekar Patha <rajashekarpatha07@gmail.com>
2025-03-21 11:25:25 +0530

    added subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import {ApiError} from "../utils/ApiError.js"
 import { User } from "../models/user.model.js";
 import { UploadOnCloudinary } from "../utils/Cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { jwt } from "jsonwebtoken";
+import  jwt  from "jsonwebtoken";
 
 const generateAccessTokenandRefreshToken = async(userId) => {
     try {
